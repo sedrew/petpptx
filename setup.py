7aa307d20dbda2e9849ccd75757c3655160d33a3
sedrew <maligin.serega2010@yandex.ru>
2023-06-06 18:20:45 +0600

    rename

--- setup.py
+++ setup.py
@@ -31,13 +31,13 @@ license = ascii_bytes_from(thisdir, "LICENSE")
 version = re.search(r'__version__ = "([^"]+)"', init_py).group(1)
 
 
-NAME = "python-petpptx"
+NAME = "petpptx"
 VERSION = version
 DESCRIPTION = "Generate and manipulate Open XML PowerPoint (.pptx) files"
 KEYWORDS = "powerpoint ppt pptx office open xml"
 AUTHOR = "Sergey Malygin"
 AUTHOR_EMAIL = "maligin.serega2010@yndex.ru"
-URL = "https://github.com/sedrew/python-petpptx"
+URL = "https://github.com/sedrew/petpptx"
 LICENSE = license
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
 PACKAGE_DATA = {"pptx": ["templates/*"]}
